15ed5b35d2d7553057d9139ed5339999924eecf9
Magnific86 <klevtsov10012002@gmail.com>
2023-03-01 20:44:46 +0500

    a

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import { Collapse, Input } from "antd";
-import { FC, useEffect, useRef, useState } from "react";
+import { ChangeEvent, FC, useEffect, useRef, useState } from "react";
 import { SwiperSlide, Swiper } from "swiper/react";
 import { categories } from "./utils/categories";
 import "swiper/css";
@@ -23,6 +23,8 @@ import { useAppContext } from "./context/MyContext";
 export const App: FC = () => {
   const { ref, inView } = useInView();
   const { width: w } = useWindowSize();
+  const [searchTerm, setSearchTerm] = useState("");
+  // const [showCount, setShowCount] = useState(0);
   const { isAdmin, setIsAdmin, getAllPosts, allPosts, setAllPosts } =
     useAppContext();
   const inpRef = useRef<HTMLInputElement>(null);
@@ -67,16 +69,9 @@ export const App: FC = () => {
     }
   };
 
-  // const getAllPosts = async () => {
-  //   try {
-  //     const data = await axios.get(`http://localhost:5000/api/posts`);
-  //     console.log("Post list updated", data.data.body);
-  //     setAllPosts(data.data.body);
-  //   } catch (e) {
-  //     toast.error("failed to update post list", e?.message);
-  //     console.error("failed to update post list", e);
-  //   }
-  // };
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
 
   // const handleEditPost = async (post: IPost) => {
   //   try {
@@ -91,13 +86,22 @@ export const App: FC = () => {
   //   }
   // };
 
+  let showCount = 0;
+
   return (
     <div className="main">
       <form className="mainSearch">
         <label htmlFor="main">
           <SearchOutlined style={{ fontSize: "50px" }} />
         </label>
-        <input id="main" className="visibleInput" ref={inpRef} type="text" />
+        <input
+          value={searchTerm}
+          onChange={handleSearch}
+          id="main"
+          className="visibleInput"
+          ref={inpRef}
+          type="text"
+        />
         <input type="submit" hidden />
       </form>
       <Swiper
@@ -125,29 +129,32 @@ export const App: FC = () => {
         ))}
       </Swiper>
       <Collapse ghost>
-        {allPosts &&
-          allPosts.map(
-            ({ _id, question, answer, category, timeCreated }, index) => (
-              <Collapse.Panel
-                header={
-                  <div className="collapseHeader">
-                    <p>{question}</p>
-                    {isAdmin && (
-                      <div className="options">
-                        <button
-                          onClick={(e) => {
-                            e.stopPropagation();
-                            handleDeletePost(_id);
-                          }}
-                        >
-                          <DeleteOutlined
-                            style={{
-                              fontSize: "20px",
-                              color: "red",
+        {allPosts?.length > 0 &&
+          allPosts.map(({ _id, question, answer, timeCreated }, index) => {
+            let regexp = new RegExp(searchTerm, "gi");
+            if (regexp.test(question)) {
+              showCount++;
+              return (
+                <Collapse.Panel
+                  header={
+                    <div className="collapseHeader">
+                      <p>{question}</p>
+                      {isAdmin && (
+                        <div className="options">
+                          <button
+                            onClick={(e) => {
+                              e.stopPropagation();
+                              handleDeletePost(_id);
                             }}
-                          />
-                        </button>
-                        {/* <button
+                          >
+                            <DeleteOutlined
+                              style={{
+                                fontSize: "20px",
+                                color: "red",
+                              }}
+                            />
+                          </button>
+                          {/* <button
                           onClick={(e) => {
                             handleEditPost({
                               _id,
@@ -161,16 +168,21 @@ export const App: FC = () => {
                         >
                           <EditOutlined style={{ fontSize: "20px" }} />
                         </button> */}
-                      </div>
-                    )}
-                  </div>
-                }
-                key={index}
-              >
-                <Post answer={answer} timeCreated={timeCreated} />
-              </Collapse.Panel>
-            )
-          )}
+                        </div>
+                      )}
+                    </div>
+                  }
+                  key={index}
+                >
+                  <Post answer={answer} timeCreated={timeCreated} />
+                </Collapse.Panel>
+              );
+            } else if (index !== 0 && showCount !== 0) {
+              return <p className="notFinded">Не найдено...</p>;
+            } else {
+              return;
+            }
+          })}
       </Collapse>
     </div>
   );
