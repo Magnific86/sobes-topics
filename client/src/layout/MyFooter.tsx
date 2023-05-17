9ce27460a3f8598861c697fc3aa940a7705f3ae7
Magnific86 <112124588+Magnific86@users.noreply.github.com>
2023-05-17 19:44:26 +0300

    upd

--- client/src/layout/MyFooter.tsx
+++ client/src/layout/MyFooter.tsx
@@ -1,32 +1,19 @@
 import { FC } from "react"
 import { ISocial } from "../globalTypes"
-import { Image } from "antd"
-import { useAppContext } from "../context/MyContext"
 import { socials } from "../utils/staticArrs/socials"
-import darkVK from "../assets/darkVk.png"
-import lightVK from "../assets/lightVk.png"
 
-export const MyFooter: FC = () => {
-  const { theme } = useAppContext()
-  return (
-    <footer>
-      <div className="divider" />
-      <div className="upperFooter">
-        <h4>Контакты, соц сети и ссылка на гитхаб:</h4>
-        <ul className="socialList">
-          {socials.map((el: ISocial) => (
-            <a key={el.url} href={el.url} target="_blank">
-              <li>
-                {el.url === "https://vk.com/klevtsov2018" ? (
-                  <Image preview={false} width={50} height={50} src={theme === "dark" ? darkVK : lightVK} alt="VK" />
-                ) : (
-                  el.icon
-                )}
-              </li>
-            </a>
-          ))}
-        </ul>
-      </div>
-    </footer>
-  )
-}
+export const MyFooter: FC = () => (
+  <footer>
+    <div className="divider" />
+    <div className="upperFooter">
+      <h4>Контакты, соц сети и ссылка на гитхаб:</h4>
+      <ul className="socialList">
+        {socials.map((el: ISocial) => (
+          <a key={el.url} href={el.url} target="_blank" rel="noreferrer">
+            <li>{el.icon}</li>
+          </a>
+        ))}
+      </ul>
+    </div>
+  </footer>
+)
--- client/src/utils/staticArrs/socials.tsx
+++ client/src/utils/staticArrs/socials.tsx
@@ -1,17 +1,13 @@
-import { FaTelegramPlane } from "@react-icons/all-files/fa/FaTelegramPlane";
-import { AiFillGithub } from "@react-icons/all-files/ai/AiFillGithub";
-import { BsEmojiSunglasses } from "react-icons/bs";
-import { InstagramOutlined, LinkedinOutlined } from "@ant-design/icons";
-import { ISocial } from "../../globalTypes";
+import { FaTelegramPlane } from "@react-icons/all-files/fa/FaTelegramPlane"
+import { AiFillGithub } from "@react-icons/all-files/ai/AiFillGithub"
+import { BsEmojiSunglasses } from "react-icons/bs"
+import { LinkedinOutlined } from "@ant-design/icons"
+import { ISocial } from "../../globalTypes"
 
 export const socials: ISocial[] = [
   {
-    icon: <InstagramOutlined />,
-    url: "https://www.instagram.com/klevtsov49/",
-  },
-  {
     icon: <BsEmojiSunglasses />,
-    url: "https://portfolio-two-brown-94.vercel.app/",
+    url: "https://portfolio-flame-kappa-68.vercel.app/",
   },
   {
     icon: <FaTelegramPlane />,
@@ -25,8 +21,4 @@ export const socials: ISocial[] = [
     icon: <LinkedinOutlined />,
     url: "https://www.linkedin.com/in/nikita-klevtsov-018a59263/",
   },
-  {
-    icon: <></>,
-    url: "https://vk.com/klevtsov2018",
-  },
-];
+]
